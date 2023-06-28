cecc60440657c3a3b29aecfd7b483639819234a0
Michelle Trinh <trinhmichelle23@gmail.com>
2023-06-27 21:50:52 -0700

    this is my finished work for Day 1

--- Day 1.playground/Contents.swift
+++ Day 1.playground/Contents.swift
@@ -1,3 +1,71 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// How to create variables and constants
+var theWeeknd = "singer"
+
+let character = "joss"
+
+greeting = "hola como estas"
+
+var playerName = "Michelle"
+print(playerName)
+
+playerName = "Jocelyn"
+print(playerName)
+
+playerName = "Selena"
+print(playerName)
+
+let actor = "Gabriel Macht"
+
+// How to create strings
+let quote = "Two roads \"Diverged\" into the road"
+
+let movie = """
+why must
+things be
+on separate lines
+"""
+
+let nameLength = (actor.count)
+print(nameLength)
+
+print(actor.uppercased())
+
+print(movie.hasPrefix("why must"))
+print(movie.hasSuffix("lines"))
+
+//How to store whole numbers
+let score = 10
+let reallyBig = 100_000_000
+
+let lowerScore = score - 2
+let squaredScore = score * score
+
+var counter = 10
+counter += 5
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+// How to store decimal numbers
+let numero = 0.1 + 0.2
+print(numero)
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+//floats in swift are called double
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var nombre = "Nick Cage"
+nombre = "Donna Paulson"
+
+var rating = 5.0
+rating *= 2
