cf13d2c4af8ec3669d89df36ca101b95fc5c4c30
Michelle Trinh <trinhmichelle23@gmail.com>
2023-06-29 20:36:30 -0700

    finished notes for day 7
    this is my commit for day 7 notes.

---
+++ Day 7.playground/Contents.swift
@@ -0,0 +1,117 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+// How to reuse code with functions
+
+func showWelcome() {
+    print("Welcome to my app!")
+    print("By default This prints out a conversion")
+    print("chart from centimeters to inches, but you")
+    print("can also set a custom range if you want.")
+}
+
+showWelcome()
+
+let number = 139
+
+if number.isMultiple(of: 2) {
+    print("Even")
+} else {
+    print("Odd")
+}
+
+let roll = Int.random(in: 1...20)
+
+func printTimesTables(number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(number: 5)
+
+func printTimesTable(number: Int, end: Int) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTable(number: 5, end: 20)
+
+// How to return values from functions
+
+let root = sqrt(169)
+print(root)
+
+func rollDice() -> Int {
+    return Int.random(in: 1...6)
+}
+
+let result = rollDice()
+print(result)
+
+func areLettersIdentical(string1: String, string2: String) -> Bool {
+    string1.sorted() == string2.sorted()
+}
+
+let c = pythagoras(a: 3, b: 4)
+print(c)
+
+func pythagoras(a: Double, b: Double) -> Double {
+    sqrt(a * a + b * b)
+}
+
+// how to return multiple values from functions
+func isUppercase(string: String) -> Bool {
+    string == string.uppercased()
+}
+
+
+func getUser() -> (firstName: String, lastName: String) {
+    (firstName: "Taylor", lastName: "Swift")
+}
+
+let (firstName, _) = getUser()
+print("Name: \(firstName)")
+
+// How to customize parameter labels
+
+func rollDice(sides: Int, count: Int) -> [Int] {
+    // Start with an empty array
+    var rolls = [Int]()
+
+    // Roll as many dice as needed
+    for _ in 1...count {
+        // Add each result to our array
+        let roll = Int.random(in: 1...sides)
+        rolls.append(roll)
+    }
+
+    // Send back all the rolls
+    return rolls
+}
+
+let rolls = rollDice(sides: 6, count: 4)
+
+func hireEmployee(name: String) { }
+func hireEmployee(title: String) { }
+func hireEmployee(location: String) { }
+
+let lyric = "I see a red door and I want it painted black"
+print(lyric.hasPrefix("I see"))
+
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let string = "HELLO, WORLD"
+let results = isUppercase(string)
+
+func printTimesTables(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5)
