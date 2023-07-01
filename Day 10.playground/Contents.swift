8fd30c01cb37b53dc93fa19b49bdac43f256b07d
Michelle Trinh <trinhmichelle23@gmail.com>
2023-06-30 20:24:21 -0700

    complete day 10 notes
    here is my commit for day 10. it includes my notes!

---
+++ Day 10.playground/Contents.swift
@@ -0,0 +1,106 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+// how to create your own structs
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+struct Employee {
+    let name: String
+    var vacationRemaining = 14
+
+   mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation!")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! There aren't enough days remaining.")
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+archer.takeVacation(days: 5)
+print(archer.vacationRemaining)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+let kane = Employee(name: "Lana Kane")
+let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
+print(poovey.vacationRemaining)
+
+// how to compute property values dynamically
+// difficult to take notes for as the code redeclares a lot of previous variables and constants
+
+
+// How to take action when a property changes
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+// how to create custome initializers
+
+struct Player {
+    let name: String
+    let number: Int
+
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
+
