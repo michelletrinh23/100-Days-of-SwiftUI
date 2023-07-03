9a306019ccbf81df1fdf3c801e873a0576c997e6
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-02 22:52:30 -0700

    finished notes for day 14
    here is my commit for day 14! it includes my notes for the day as well as my work for checkpoint 9.

---
+++ Day 14.playground/Contents.swift
@@ -0,0 +1,112 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+// how to handle missing data with optionals
+let opposites = [
+    "Mario": "Wario",
+    "Luigi": "Waluigi"
+]
+
+let peachOpposite = opposites["Peach"]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
+
+var username: String? = nil
+
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
+}
+
+func square(number: Int) -> Int {
+    number * number
+}
+
+var number: Int? = nil
+
+
+if let unwrappedNumber = number {
+    print(square(number: unwrappedNumber))
+}
+
+// how to unwrap optionals with guard
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+
+        // 1: We *must* exit the function here
+        return
+    }
+
+    // 2: `number` is still available outside of `guard`
+    print("\(number) x \(number) is \(number * number)")
+}
+
+// how to unwrap optionals with nil coalescing
+
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+let new = captains["Serenity", default: "N/A"]
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+// how to handle multiple optionals using optional chaining
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+struct Books {
+    let titles: String
+    let authors: String?
+}
+
+var books: Books? = nil
+let authors = books?.authors?.first?.uppercased() ?? "A"
+print(authors)
+
+// how to handle function failiure with optionals
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
+
+// checkpoint 9
+
+let numeros: [Int] = []
+    
+func getRandomElement(_ numeros: [Int]?) -> Int { return numeros?.randomElement() ?? Int.random(in: 1...100) }
+
+let randomElement = getRandomElement(numeros)
+print("Random Element:", randomElement)
