5850290c8fc8ee542f55642d55598089f21b6c6f
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-04 00:22:14 -0700

    Day 16 Commit
    Here is my commit for Day 16. There were 7 tutorials to follow and I followed along in my code, but at the end of the day 16 tutorials, the instructor said to return the code back to its original form. Therefore, this commit is essentially just a clean slate of starting code.

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Michelle Trinh on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
