b5f218efcba05be7b8597e7cb2c903df79be01f4
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-07 14:44:11 -0700

    Day 20 Commit
    Here is my commit for Day 20. There were 6 topics to follow and I followed along in my code, but at the end of the day 20 tutorials, the instructor said to return the code back to its original form. Therefore, this commit is essentially just a clean slate of starting code.

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Michelle Trinh on 7/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Michelle Trinh on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
