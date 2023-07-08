10bf7c755c6e1f331060110b9633eeb94a61c775
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-07 21:49:14 -0700

    Day 22 Commit
    Hello! Here is my commit for day 22 of the 100 days of SwiftUI. I did some challenges which involved making changes to the GuessTheFlag app.

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,9 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
-    
+    @State private var userScore = 0
+    @State private var gameOver = false
+    @State private var gameState = ""
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var tapCounter = 0
     
     var body: some View {
         ZStack {
@@ -57,7 +60,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
@@ -68,24 +71,47 @@ struct ContentView: View {
             .alert(scoreTitle, isPresented: $showingScore) {
                 Button("Continue", action: askQuestion)
             } message: {
-                Text("Your score is ???")
+                Text("Your score is \(userScore)")
+            }
+            .alert(gameState, isPresented: $gameOver) {
+                Button("Start Over", action: restartGame)
+            } message: {
+                Text("Game Over! Your final score is \(userScore)")
             }
         }
     
     func flagTapped(_ number: Int) {
+        tapCounter += 1
+        
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[number]) "
+            userScore -= 1
         }
         
         showingScore = true
+        
+        if tapCounter >= 8 {
+                    gameOver = true
+                    gameState = "Game Over"
+                }
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    
+    func restartGame() {
+            tapCounter = 0
+            userScore = 0
+            gameOver = false
+            gameState = ""
+            askQuestion()
+        }
+    
 }
 
 #Preview {
