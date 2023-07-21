cf4a856ab3f7abd14ab7b8b43811eb420219431b
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-21 13:00:17 -0700

    Day 26 Topic 2
    This is my commit for the second topic of day 26, selecting dates and times with DatePicker.

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
 
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
     }
+    
+    func exampleDates() {
+        // create a second Date instance set to one day in seconds from now
+        let tomorrow = Date.now.addingTimeInterval(86400)
+
+        // create a range from those two
+        let range = Date.now...tomorrow
+    }
+    
 }
 
 #Preview {
