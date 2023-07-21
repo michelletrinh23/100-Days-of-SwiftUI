39d1ebc46811fbef338149aaf31283ddbbfc370f
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-20 23:32:56 -0700

    Day 25 Commit
    final commit for day 25! The challenge is complete. Most of the changes in this commit are for the working features of the game.

--- BrainTraining/BrainTraining/ContentView.swift
+++ BrainTraining/BrainTraining/ContentView.swift
@@ -14,14 +14,17 @@ struct ChoiceImage: View {
         switch choice {
             case "Rock":
                 Text("🪨")
+                    .font(.system(size: 120))
                     .foregroundStyle(.tint)
                     .shadow(radius: 5)
             case "Paper":
                 Text("📄")
+                    .font(.system(size: 120))
                     .foregroundStyle(.tint)
                     .shadow(radius: 5)
             case "Scissors":
                 Text("✂️")
+                    .font(.system(size: 120))
                     .foregroundStyle(.tint)
                     .shadow(radius: 5)
         default:
@@ -40,23 +43,25 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var userScore = 0
     @State private var showingScore = false
-    
+    @State private var winObjective = true
     var body: some View {
         VStack {
             Spacer()
             
             Text("Rock, Paper, or Scissors?")
                 .font(.largeTitle.bold())
-                .foregroundColor(.black)
+                .foregroundColor(.primary)
             
             VStack(spacing: 15){
                 VStack {
                     Text("Make Your Choice")
                         .foregroundStyle(.secondary)
                         .font(.subheadline.weight(.heavy))
-                    Text("Win this time")
+    
+                    Text(winObjective ? "Win this time" : "Lose this time")
                         .font(.largeTitle.weight(.semibold))
                 }
+                
                 ForEach(0..<3) { number in
                     Button {
                         choiceTapped(number)
@@ -70,28 +75,68 @@ struct ContentView: View {
             .background(.regularMaterial)
             .clipShape(RoundedRectangle(cornerRadius: 20))
             
+            Spacer()
+            Spacer()
+            
+            Text("Score: \(userScore)")
+                .foregroundColor(.primary)
+                .font(.title.bold())
             
             Spacer()
+            
+            if showingScore {
+                Text(scoreTitle)
+            }
         }
         .padding()
+        .alert(gameState, isPresented: $gameOver) {
+            Button("Start Over", action: restartGame)
+        } message: {
+            Text("Game Over! Your final score is \(userScore)")
+        }
     }
+        
     
     func choiceTapped(_ number: Int) {
         tapCounter += 1
         
         let opponentChoice = choices[Int.random(in: 0..<3)]
         
-        if number == correctAnswer {
-            scoreTitle = "Correct! Congratulations!"
-            userScore += 1
-        } else {
-            scoreTitle = "Wrong! You should have chosen \(choices[number]) "
-            userScore -= 1
-        }
+        if winObjective {
+            if (number == 0 && opponentChoice == "Rock") ||
+                (number == 1 && opponentChoice == "Paper") ||
+                (number == 2 && opponentChoice == "Scissors") {
+                scoreTitle = "Opponent chose \(opponentChoice). It's a tie!"
+                userScore += 1
+            } else if (number == 0 && opponentChoice == "Scissors") ||
+                   (number == 1 && opponentChoice == "Rock") ||
+                   (number == 2 && opponentChoice == "Paper") {
+                    scoreTitle = "Opponent chose \(opponentChoice). You are correct! Congratulations!"
+                    userScore += 1
+                } else {
+                    scoreTitle = "Opponent chose \(opponentChoice). You chose wrong!"
+                    userScore -= 1
+                }
+            } else {
+                if (number == 0 && opponentChoice == "Rock") ||
+                    (number == 1 && opponentChoice == "Paper") ||
+                    (number == 2 && opponentChoice == "Scissors") {
+                    scoreTitle = "Opponent chose \(opponentChoice). It's a tie!"
+                    userScore += 1
+                } else if (number == 0 && opponentChoice == "Paper") ||
+                   (number == 1 && opponentChoice == "Scissors") ||
+                   (number == 2 && opponentChoice == "Rock") {
+                    scoreTitle = "Opponent chose \(opponentChoice). You are correct! Congratulations!"
+                    userScore += 1
+                } else {
+                    scoreTitle = "Opponent chose \(opponentChoice). You chose wrong!"
+                    userScore -= 1
+                }
+            }
         
         showingScore = true
         
-        
+        winObjective.toggle()
         
         if tapCounter >= 10 {
                     gameOver = true
@@ -99,6 +144,18 @@ struct ContentView: View {
                 }
     }
     
+    func askQuestion() {
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func restartGame() {
+            tapCounter = 0
+            userScore = 0
+            gameOver = false
+            gameState = ""
+            askQuestion()
+            scoreTitle = ""
+        }
 }
 
 
