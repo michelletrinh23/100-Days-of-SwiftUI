cfc4ed27541f2062d4a5d1f5431c909d67e32bb1
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-21 15:32:04 -0700

    Day 27 topic 1
    this commit includes edits to the code in BetterRest. Building a basic layout for the app.

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,20 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
-
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-    }
-    
-    func exampleDates() {
-        // create a second Date instance set to one day in seconds from now
-        let tomorrow = Date.now.addingTimeInterval(86400)
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
 
-        // create a range from those two
-        let range = Date.now...tomorrow
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+
+                Text("Desired amount of sleep")
+                    .font(.headline)
+
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+        }
     }
-    
+    func calculateBedtime() {
+    }
+
 }
+    
+
 
 #Preview {
     ContentView()
