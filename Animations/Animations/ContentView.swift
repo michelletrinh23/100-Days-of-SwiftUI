52875b650f6e96c43fd202a9050fc38c04e185cb
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-26 16:36:40 -0700

    Day 32 topic 4
    this commit contains changes to the Animations app that have notes on creating explicit animations.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,28 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
 
     var body: some View {
-        print(animationAmount)
-
-        return VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(
-                .easeInOut(duration: 1)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-
-            Spacer()
-
-            Button("Tap Me") {
-                animationAmount += 1
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
             }
-            .padding(40)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
         }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+
     }
 }
 
