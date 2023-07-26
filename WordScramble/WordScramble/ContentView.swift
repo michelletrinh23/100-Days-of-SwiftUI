e12e1e1d7632d6f0d3914e9e6d7634d750f64cc0
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-25 17:26:23 -0700

    Day 29 topic 2
    this commit includes notes for topic 2 of day 29 in 100 days of swiftUI (loading resources from an app bundle)

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
-
     var body: some View {
-        List {
-            Text("Static Row")
-            
-            ForEach(people, id: \.self) {
-                Text($0)
+        Text("Hello, world!")
+            .padding()
+    }
+    
+    func loadFile() {
+        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+            // we found the file in our bundle!
+            if let fileContents = try? String(contentsOf: fileURL) {
+                // we loaded the file into a string!
             }
-            
-            Text("Static Row")
         }
     }
 }
