4690df46195148694818c11085b215f79e0568c7
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-26 20:19:55 -0700

    Day 33 topic 2
    this commit contains edits to the Animations app for animating gestures.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,20 +7,55 @@
 
 import SwiftUI
 
+/*
 struct ContentView: View {
-    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+
     
     var body: some View {
-        Button("Tap Me") {
-            enabled.toggle()
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.spring()) {
+                            dragAmount = .zero
+                        }
+                    }
+            )
+            .animation(.spring(), value: dragAmount)
+
+    }
+}
+*/
+
+struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+            }
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-        .animation(nil, value: enabled)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
-        
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
