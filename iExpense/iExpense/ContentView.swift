289f89e162df36165cdab8b8e3f1322fe84d0de9
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-31 12:59:02 -0700

    Day 36 Topic 4
    This commit includes notes for deleting items using onDelete().

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,28 +7,35 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
 
     var body: some View {
-        Button("Dismiss") {
-            dismiss()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                    
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+        }
+        .navigationTitle("onDelete()")
+        .toolbar {
+            EditButton()
         }
     }
-}
-
-struct ContentView: View {
-    @State private var showingSheet = false
-
-    var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "@twostraws")
-        }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
