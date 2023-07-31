54a7fe3dcdf7edbf98fa224aa0375f4551da4ce0
Michelle Trinh <trinhmichelle23@gmail.com>
2023-07-31 12:03:30 -0700

    Day 36 Topic 1
    This commit includes notes on why @State only works with structs.

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Michelle Trinh on 7/31/23.
+//
+
+import SwiftUI
+
+struct User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
+struct ContentView: View {
+    @State private var user = User()
+
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
+
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Michelle Trinh on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
