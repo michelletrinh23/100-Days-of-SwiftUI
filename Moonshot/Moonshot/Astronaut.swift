f0ee0ea8f2ca1d4d6b6271ad00d7c106634c1eff
Michelle Trinh <trinhmichelle23@gmail.com>
2023-08-01 22:38:47 -0700

    Day 40 Topic 1
    This commit includes changes to the Moonshot app for loading a specific kind of Codable data.

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Michelle Trinh on 8/1/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,32 +7,15 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
-
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120)),
-    ]
-    
+    let astronauts = Bundle.main.decode("astronauts.json")
+
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
-            }
-        }
+        Text("\(astronauts.count)")
+        .padding()
     }
 }
- 
+
 #Preview {
     ContentView()
 }
