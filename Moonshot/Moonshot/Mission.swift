515bd982a4fb2bde79c398158b7de9083db84b70
Michelle Trinh <trinhmichelle23@gmail.com>
2023-08-02 00:29:43 -0700

    Day 40 Topic 2
    This commit includes changes to the Moonshot app for using generics to load any kind of Codable data.

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Michelle Trinh on 8/2/23.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
