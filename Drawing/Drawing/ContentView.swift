262ee5b03b6460b54875e8bd7025f9ef8ad162e9
Michelle Trinh <trinhmichelle23@gmail.com>
2023-08-09 23:57:13 -0700

    Day 45 Topic 1
    This commit includes notes on special effects in SwiftUI, such as blurs, blending, and saturation.

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,52 +7,24 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: Double(value))
-                    .strokeBorder(
-                        LinearGradient(
-                            gradient: Gradient(colors: [
-                                color(for: value, brightness: 1),
-                                color(for: value, brightness: 0.5)
-                            ]),
-                            startPoint: .top,
-                            endPoint: .bottom
-                        ),
-                        lineWidth: 2
-                    )
-            }
-        }
-        .drawingGroup()
-    }
-
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(steps) + amount
-
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
-    }
-}
-
 struct ContentView: View {
-    @State private var colorCycle = 0.0
+    @State private var amount = 0.0
 
     var body: some View {
         VStack {
-            ColorCyclingCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
-
-            Slider(value: $colorCycle)
+            Image("Example")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .saturation(amount)
+                .blur(radius: (1 - amount) * 20)
+            
+            Slider(value: $amount)
+                .padding()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
     }
 }
 
