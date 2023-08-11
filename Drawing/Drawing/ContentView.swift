64813f14ca8e7fc2b352883ce51962c9caa0ab55
Michelle Trinh <trinhmichelle23@gmail.com>
2023-08-11 13:54:42 -0700

    Day 46 Challenge 1
    This commit includes changes to the Drawing app. I created an Arrow shape in a struct.

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,58 +7,26 @@
 
 import SwiftUI
 
-struct Checkerboard: Shape {
-    var rows: Int
-    var columns: Int
-    
-    var animatableData: AnimatablePair<Double, Double> {
-        get {
-           AnimatablePair(Double(rows), Double(columns))
-        }
-
-        set {
-            rows = Int(newValue.first)
-            columns = Int(newValue.second)
-        }
-    }
-
+struct Arrow: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
-
-        // figure out how big each row/column needs to be
-        let rowSize = rect.height / Double(rows)
-        let columnSize = rect.width / Double(columns)
-
-        // loop over all rows and columns, making alternating squares colored
-        for row in 0..<rows {
-            for column in 0..<columns {
-                if (row + column).isMultiple(of: 2) {
-                    // this square should be colored; add a rectangle here
-                    let startX = columnSize * Double(column)
-                    let startY = rowSize * Double(row)
-
-                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
-                    path.addRect(rect)
-                }
-            }
-        }
-
+        
+        path.move(to: CGPoint(x: 200, y: 150))
+        path.addLine(to: CGPoint(x: 300, y: 300))
+        path.move(to: CGPoint(x: 200, y: 150))
+        path.addLine(to: CGPoint(x: 100, y: 300))
+        path.move(to: CGPoint(x: 200, y: 150))
+        path.addLine(to: CGPoint(x: 200, y: 600))
+        
         return path
     }
 }
 
 struct ContentView: View {
-    @State private var rows = 4
-    @State private var columns = 4
 
     var body: some View {
-        Checkerboard(rows: rows, columns: columns)
-            .onTapGesture {
-                withAnimation(.linear(duration: 3)) {
-                    rows = 8
-                    columns = 16
-            }
-        }
+        Arrow()
+            .stroke()
     }
 }
 
