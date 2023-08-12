3580606c36ca844a6640be61926601b7d65695cd
Michelle Trinh <trinhmichelle23@gmail.com>
2023-08-11 20:04:24 -0700

    Day 46 Challenge 3
    This commit includes changes to the Drawing app for a ColorCyclingRectangle shape with various adjustable properties.

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -11,33 +11,111 @@ struct Arrow: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: 200, y: 150))
-        path.addLine(to: CGPoint(x: 300, y: 300))
-        path.move(to: CGPoint(x: 200, y: 150))
-        path.addLine(to: CGPoint(x: 100, y: 300))
-        path.move(to: CGPoint(x: 200, y: 150))
-        path.addLine(to: CGPoint(x: 200, y: 600))
+        path.move(to: CGPoint(x: 250, y: 100))
+        path.addLine(to: CGPoint(x: 200, y: 50))
+        path.addLine(to: CGPoint(x: 150, y: 100))
+        path.move(to: CGPoint(x: 200, y: 50))
+        path.addLine(to: CGPoint(x: 200, y: 150))
         
         return path
     }
 }
 
-struct ContentView: View {
-    @State private var arrowWidth = 5.0
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
+    var verticalStart: Double
+    var horizontalStart: Double
+    var verticalEnd: Double
+    var horizontalEnd: Double
     
     var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: UnitPoint(x: horizontalStart, y: verticalStart),
+                            endPoint: UnitPoint(x: horizontalEnd, y: verticalEnd)
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+
+struct ContentView: View {
+    @State private var arrowWidth = 5.0
+    @State private var colorCycle = 0.0
+    @State private var vertical = 0.0
+    @State private var horizontal = 0.0
+    
+    @State var verticalStart = 0.0
+    @State var horizontalStart = 0.0
+    @State var verticalEnd = 0.0
+    @State var horizontalEnd = 0.0
+
+    var body: some View {
         
-        Text("Press on the arrow to receive a random arrow width!")
-            .multilineTextAlignment(.center)
-            .font(.title)
-        Arrow()
-            .stroke(lineWidth: CGFloat(arrowWidth))
-            .onTapGesture {
-                withAnimation {
-                    arrowWidth = Double.random(in: 1...50)
+            Text("Press on the arrow to receive a random arrow width!")
+                .multilineTextAlignment(.center)
+                .font(.title)
+            Arrow()
+                .stroke(lineWidth: CGFloat(arrowWidth))
+                .onTapGesture {
+                    withAnimation {
+                        arrowWidth = Double.random(in: 1...50)
+                    }
+                }
+            Text("Current arrow width: \(arrowWidth)")
+            
+            Divider()
+        ScrollView {
+            VStack {
+                ColorCyclingRectangle(amount: colorCycle, verticalStart: verticalStart, horizontalStart: horizontalStart, verticalEnd: verticalEnd, horizontalEnd: horizontalEnd)
+                    .frame(width: 300, height: 300)
+                    .padding()
+                
+                Text("Color cycle slider!")
+                Slider(value: $colorCycle)
+                Text("Where should the gradient start?")
+                HStack {
+                    Text("X-Axis")
+                    Slider(value: $horizontalStart)
+                }
+                HStack {
+                    Text("Y-Axis")
+                    Slider(value: $verticalStart)
+                }
+                Text("Where should the gradient end?")
+                HStack {
+                    Text("X-Axis")
+                    Slider(value: $horizontalEnd)
+                }
+                HStack {
+                    Text("Y-Axis")
+                    Slider(value: $verticalEnd)
                 }
             }
-        Text("Current arrow width: \(arrowWidth)")
+        }
     }
 }
 
@@ -45,3 +123,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+ 
