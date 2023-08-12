f98c10d94019227b85d63d1106e7e3a82d16628e
Michelle Trinh <trinhmichelle23@gmail.com>
2023-08-11 17:06:40 -0700

    Day 46 Challenge 2
    This commit includes changes to the Drawing app that animates the width of the arrow.

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -23,10 +23,21 @@ struct Arrow: Shape {
 }
 
 struct ContentView: View {
-
+    @State private var arrowWidth = 5.0
+    
     var body: some View {
+        
+        Text("Press on the arrow to receive a random arrow width!")
+            .multilineTextAlignment(.center)
+            .font(.title)
         Arrow()
-            .stroke()
+            .stroke(lineWidth: CGFloat(arrowWidth))
+            .onTapGesture {
+                withAnimation {
+                    arrowWidth = Double.random(in: 1...50)
+                }
+            }
+        Text("Current arrow width: \(arrowWidth)")
     }
 }
 
