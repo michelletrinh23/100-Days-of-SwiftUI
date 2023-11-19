78e6509a40380ea156f53e1c06d869bdc794b788
Michelle Trinh <trinhmichelle23@gmail.com>
2023-11-18 16:31:02 -0800

    Day 49 Topic 3
    This commit includes changes to the CupcakeCorner app for validating and disabling forms.

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -9,19 +9,27 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image.")
-            } else {
-                ProgressView()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }
+            .disabled(disableForm)
         }
-        .frame(width: 200, height: 200)
     }
 }
 
