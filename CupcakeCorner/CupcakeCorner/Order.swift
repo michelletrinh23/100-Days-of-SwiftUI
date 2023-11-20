762633a8a9279268c859aa3531e3981b83668710
Michelle Trinh <trinhmichelle23@gmail.com>
2023-11-19 22:24:24 -0800

    Day 51 Topic 3
    This commit includes changes to the CupcakeCorner app for sending and receiving orders over the internet.

--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -8,7 +8,19 @@
 import Foundation
 
 @Observable
-class Order {
+class Order: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
 
     var type = 0
