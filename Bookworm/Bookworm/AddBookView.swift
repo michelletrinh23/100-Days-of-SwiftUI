4bd44660f84b0c20e571ee786f497c7fa2c6dd20
Michelle Trinh <trinhmichelle23@gmail.com>
2023-11-29 22:39:30 -0800

    Day 54 Topic 2
    This commit includes changes to the Bookworm app for adding a custom star rating component.

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -35,14 +35,9 @@ struct AddBookView: View {
 
                 Section("Write a review") {
                     TextEditor(text: $review)
-
-                    Picker("Rating", selection: $rating) {
-                        ForEach(0..<6) {
-                            Text(String($0))
-                        }
-                    }
+                    RatingView(rating: $rating)
                 }
-
+                
                 Section {
                     Button("Save") {
                         let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
