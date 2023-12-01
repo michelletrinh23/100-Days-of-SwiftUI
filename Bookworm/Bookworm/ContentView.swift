3d5aea97d6431c8477e0974f899632ac0ce28988
Michelle Trinh <trinhmichelle23@gmail.com>
2023-11-30 22:31:55 -0800

    Day 55 Topic 3
    This commit includes changes to the Bookworm app for deleting from a SwiftData query.

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -35,12 +35,17 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
            .navigationTitle("Bookworm")
            .navigationDestination(for: Book.self) { book in
                DetailView(book: book)
            }
            .toolbar {
+               ToolbarItem(placement: .topBarLeading) {
+                   EditButton()
+               }
+               
                ToolbarItem(placement: .topBarTrailing) {
                    Button("Add Book", systemImage: "plus") {
                        showingAddScreen.toggle()
@@ -51,6 +56,16 @@ struct ContentView: View {
                AddBookView()
            }
        }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our query
+            let book = books[offset]
+
+            // delete it from the context
+            modelContext.delete(book)
+        }
     }
 }
 
