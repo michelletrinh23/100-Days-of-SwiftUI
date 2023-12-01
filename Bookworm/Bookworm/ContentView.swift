32a08bfd1c0c1f87b9780a7253d3aa52af1a54dd
Michelle Trinh <trinhmichelle23@gmail.com>
2023-11-30 22:01:39 -0800

    Day 55 Topic 2
    This commit includes changes to the Bookworm app for sorting SwiftData queries using SortDescriptor.

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -10,8 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
-
+    @Query(sort: [
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author)
+    ]) var books: [Book]
+    
     @State private var showingAddScreen = false
     
     var body: some View {
