76bc39576e92f1bfae024f92498f3319a26a8aef
Michelle Trinh <trinhmichelle23@gmail.com>
2023-12-04 23:03:29 -0800

    Day 56 Challenge 2
    This commit includes changes to the Bookworm app in the ContentView so the books rated as 1 star have their title shown in red.

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
                             VStack(alignment: .leading) {
                                 Text(book.title)
                                     .font(.headline)
+                                    .foregroundColor(book.rating == 1 ? .red : .primary)
                                 Text(book.author)
                                     .foregroundStyle(.secondary)
                             }
