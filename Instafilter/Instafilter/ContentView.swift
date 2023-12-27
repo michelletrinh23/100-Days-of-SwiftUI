728e50713c3600a1c7eaaedf615fb0b39c97d895
Michelle Trinh <trinhmichelle23@gmail.com>
2023-12-27 13:12:26 -0800

    Day 62 Topic 2
    This commit includes changes to the Instafilter app for responding to state changes using onChange().

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,22 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0 {
-        didSet {
-            print("New value is \(blurAmount)")
-        }
-    }
-    
+    @State private var blurAmount = 0.0
+
     var body: some View {
         VStack {
             Text("Hello, World!")
                 .blur(radius: blurAmount)
 
             Slider(value: $blurAmount, in: 0...20)
-
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
-            }
+                .onChange(of: blurAmount) { oldValue, newValue in
+                    print("New value is \(newValue)")
+                }
         }
     }
 }
