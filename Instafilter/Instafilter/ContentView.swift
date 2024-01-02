46a8e51231a4a1c7ded40ad322971bcebddf461e
Michelle Trinh <trinhmichelle23@gmail.com>
2024-01-01 23:32:18 -0800

    Day 63 Topic 2
    This commit includes changes to the Instafilter app for showing empty states with ContentUnavailableView.

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,49 +5,20 @@
 //  Created by Michelle Trinh on 12/27/23.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
+        ContentUnavailableView {
+            Label("No snippets", systemImage: "swift")
+        } description: {
+            Text("You don't have any saved snippets yet.")
+        } actions: {
+            Button("Create Snippet") {
+                // create a snippet
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .onAppear(perform: loadImage)
-    }
-
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
-        
-        currentFilter.inputImage = beginImage
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
-               
-        // get a CIImage from our filter or exit if that fails
-        guard let outputImage = currentFilter.outputImage else { return }
-
-        // attempt to get a CGImage from our CIImage
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-
-        // convert that to a UIImage
-        let uiImage = UIImage(cgImage: cgImage)
-
-        // and convert that to a SwiftUI image
-        image = Image(uiImage: uiImage)
     }
 }
 
