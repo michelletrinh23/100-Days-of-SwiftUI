09d8f6dc962b363caac48ccf80f6fa5e1ce2f354
Michelle Trinh <trinhmichelle23@gmail.com>
2024-01-02 01:11:05 -0800

    Day 64 Topic 2
    This commit includes changes to the Instafilter app for letting the user share content with ShareLink.

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,37 +5,14 @@
 //  Created by Michelle Trinh on 12/27/23.
 //
 
-import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var pickerItems = [PhotosPickerItem]()
-    @State private var selectedImages = [Image]()
-    
     var body: some View {
-        VStack {
-            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
-                Label("Select a picture", systemImage: "photo")
-            }
-            
-            ScrollView {
-                ForEach(0..<selectedImages.count, id: \.self) { i in
-                    selectedImages[i]
-                        .resizable()
-                        .scaledToFit()
-                }
-            }
-        }
-        .onChange(of: pickerItems) {
-            Task {
-                selectedImages.removeAll()
+        let example = Image(.example)
 
-                for item in pickerItems {
-                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
-                        selectedImages.append(loadedImage)
-                    }
-                }
-            }
+        ShareLink(item: example, preview: SharePreview("Singapore Airport", image: example)) {
+            Label("Click to share", systemImage: "airplane")
         }
     }
 }
