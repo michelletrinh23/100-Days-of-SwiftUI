6619105cd9ffcd11eed91406019a48c11f624bc0
Michelle Trinh <trinhmichelle23@gmail.com>
2024-01-02 22:54:03 -0800

    Day 65 Topic 2
    This commit includes changes to the Instafilter app for importing an image into SwiftUI using PhotosPicker.

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,25 +5,29 @@
 //  Created by Michelle Trinh on 12/27/23.
 //
 
-import StoreKit
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
     
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
-
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Import a photo to get started"))
+                    }
                 }
+                .buttonStyle(.plain)
                 
                 Spacer()
 
@@ -48,6 +52,15 @@ struct ContentView: View {
     
     func changeFilter() {
         
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+
+            // more code to come
+        }
     }
 }
 
