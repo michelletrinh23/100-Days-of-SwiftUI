4331cac503368b533601e7a9b94254be55ee273f
Michelle Trinh <trinhmichelle23@gmail.com>
2024-01-03 21:27:17 -0800

    Day 68 Topic 1
    This commit includes additions to the BucketList app for adding conformance to Comparable for custom types.

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Michelle Trinh on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/BucketList/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Michelle Trinh on 1/3/24.
+//
+
+import SwiftUI
+
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
+struct ContentView: View {
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister"),
+    ].sorted()
+
+    var body: some View {
+        List(users) { user in
+            Text("\(user.lastName), \(user.firstName)")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
