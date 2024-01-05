d264a66301a1dc1443aef9b443774289a566e4bf
Michelle Trinh <trinhmichelle23@gmail.com>
2024-01-04 23:22:16 -0800

    Day 71 Topic 2
    This commit includes changes to the BucketList app for sorting Wikipedia results.

--- BucketList/BucketList/EditView.swift
+++ BucketList/BucketList/EditView.swift
@@ -37,7 +37,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
-                            Text("Page description here")
+                            Text(page.description)
                                 .italic()
                         }
                     case .loading:
@@ -88,7 +88,7 @@ struct EditView: View {
             let items = try JSONDecoder().decode(Result.self, from: data)
 
             // success – convert the array values to our pages array
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             // if we're still here it means the request failed somehow
