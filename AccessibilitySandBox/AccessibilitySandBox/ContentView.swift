0e70262e27fc901b75d075f9188549caa2c36f46
Michelle Trinh <trinhmichelle23@gmail.com>
2024-01-09 00:12:33 -0800

    Day 74 Topic 3
    This commit includes changes to the AccessibilitySandBox app for reading the value of controls.

--- AccessibilitySandBox/AccessibilitySandBox/ContentView.swift
+++ AccessibilitySandBox/AccessibilitySandBox/ContentView.swift
@@ -8,16 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 10
+
     var body: some View {
         VStack {
-            VStack {
-                Text("Your score is")
-                Text("1000")
-                    .font(.title)
+            Text("Value: \(value)")
+
+            Button("Increment") {
+                value += 1
             }
-            .accessibilityElement(children: .ignore)
-            //parameter for accessibilityElement can be empty, will still be considered as .ignore
-            .accessibilityLabel("Your score is 1000")
+
+            Button("Decrement") {
+                value -= 1
+            }
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled.")
+            }
         }
     }
 }
