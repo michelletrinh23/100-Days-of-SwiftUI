ec3625043dac622797123528fcc0771610ab7b51
Michelle Trinh <trinhmichelle23@gmail.com>
2024-01-09 00:07:39 -0800

    Day 74 Topic 2
    This commit includes changes to the AccessibilitySandBox for hiding and grouping accessibility data.

--- AccessibilitySandBox/AccessibilitySandBox/ContentView.swift
+++ AccessibilitySandBox/AccessibilitySandBox/ContentView.swift
@@ -8,34 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks",
-    ]
-
-    @State private var selectedPicture = Int.random(in: 0...3)
-
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-                selectedPicture = Int.random(in: 0...3)
+        VStack {
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
             }
-            .accessibilityLabel(labels[selectedPicture])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
+            .accessibilityElement(children: .ignore)
+            //parameter for accessibilityElement can be empty, will still be considered as .ignore
+            .accessibilityLabel("Your score is 1000")
+        }
     }
 }
+
 #Preview {
     ContentView()
 }
