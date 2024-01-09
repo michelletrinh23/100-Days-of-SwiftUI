a340012e66b3628e05dee12572aef9249276c546
Michelle Trinh <trinhmichelle23@gmail.com>
2024-01-09 00:00:34 -0800

    Day 74 Topic 1
    This commit is the start of a new app called AccessibilitySandBox and contains additions for identifying views with useful labels.

---
+++ AccessibilitySandBox/AccessibilitySandBox/AccessibilitySandBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandBoxApp.swift
+//  AccessibilitySandBox
+//
+//  Created by Michelle Trinh on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandBox/AccessibilitySandBox/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  AccessibilitySandBox
+//
+//  Created by Michelle Trinh on 1/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
+
+    @State private var selectedPicture = Int.random(in: 0...3)
+
+    var body: some View {
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                selectedPicture = Int.random(in: 0...3)
+            }
+            .accessibilityLabel(labels[selectedPicture])
+            .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
+    }
+}
+#Preview {
+    ContentView()
+}
