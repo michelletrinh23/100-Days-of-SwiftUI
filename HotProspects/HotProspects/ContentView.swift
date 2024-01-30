6a6230e79c879de2eb81b2163a0dba03e8d2fc52
Michelle Trinh <trinhmichelle23@gmail.com>
2024-01-30 00:27:39 -0800

    Day 80 Topic 1
    This commit includes changes to the HotProspects app for manually publishing ObservableObject changes.

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,25 +7,27 @@
 
 import SwiftUI
 
+@MainActor class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
+
 struct ContentView: View {
-    @State private var selectedTab = "One"
+    @StateObject var updater = DelayedUpdater()
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    selectedTab = "Two"
-                }
-                .tabItem {
-                    Label("One", systemImage: "star")
-                }
-
-            Text("Tab 2")
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }
-                .tag("Two")
-        }
+        Text("Value is: \(updater.value)")
     }
 }
 
