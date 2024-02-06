a320fb35b1dcefb0a1a2855e2fa08e4a208ce8fe
Michelle Trinh <trinhmichelle23@gmail.com>
2024-02-06 00:01:29 -0800

    Day 81 Topic 3
    This commit includes changes to the HotProspects app for adding Swift package dependencies in Xcode.

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -5,42 +5,23 @@
 //  Created by Michelle Trinh on 1/19/24.
 //
 
+import SamplePackage
 import SwiftUI
-import UserNotifications
+
 
 struct ContentView: View {
-
+    let possibleNumbers = 1...60
+    
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.formatted()
+    }
+    
     var body: some View {
-        VStack {
-            Button("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-
-            Button("Schedule Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the cat"
-                content.subtitle = "It looks hungry"
-                content.sound = UNNotificationSound.default
-
-                // show this notification five seconds from now
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-                // choose a random identifier
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-                // add our notification request
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
+        Text(results)
     }
 }
-
 #Preview {
     ContentView()
 }
