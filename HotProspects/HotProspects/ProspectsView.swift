c9836a14079ac708b809209dfbab7e9132750d92
Michelle Trinh <trinhmichelle23@gmail.com>
2024-02-18 19:27:50 -0800

    Day 85 Challenge 1
    This commit includes changes to the HotProspects app for adding an icon to the "Everyone" screen showing whether a prospect was contacted or not.

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -36,11 +36,18 @@ struct ProspectsView: View {
     var body: some View {
         NavigationStack {
             List(prospects, selection: $selectedProspects) { prospect in
-                VStack(alignment: .leading) {
-                    Text(prospect.name)
-                        .font(.headline)
-                    Text(prospect.emailAddress)
-                        .foregroundStyle(.secondary)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+
+                    if filter == .none && prospect.isContacted {
+                        Spacer()
+                        Image(systemName: "checkmark.circle.fill")
+                    }
                 }
                 .swipeActions {
                     Button("Delete", systemImage: "trash", role: .destructive) {
@@ -62,6 +69,7 @@ struct ProspectsView: View {
                             addNotification(for: prospect)
                         }
                         .tint(.orange)
+                        
                     }
                 }
                 .tag(prospect)
