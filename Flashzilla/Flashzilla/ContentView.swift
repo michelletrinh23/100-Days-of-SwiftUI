3e42283a89cde3eb5e72eba530b183ab6ac8858e
Michelle Trinh <trinhmichelle23@gmail.com>
2024-02-24 22:30:15 -0800

    Day 86 Topic 1
    This commit includes additions to the Flashzilla app for using gestures in SwiftUI.

---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Michelle Trinh on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // how far the circle has been dragged
+    @State private var offset = CGSize.zero
+
+    // whether it is currently being dragged or not
+    @State private var isDragging = false
+
+    var body: some View {
+        // a drag gesture that updates offset and isDragging as it moves around
+        let dragGesture = DragGesture()
+            .onChanged { value in offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+
+        // a long press gesture that enables isDragging
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+
+        // a combined gesture that forces the user to long press then drag
+        let combined = pressGesture.sequenced(before: dragGesture)
+
+        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
+        Circle()
+            .fill(.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Michelle Trinh on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
