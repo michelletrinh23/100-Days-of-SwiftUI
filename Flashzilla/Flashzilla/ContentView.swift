226aee35bba6f09b695408114d8f32317a712b48
Michelle Trinh <trinhmichelle23@gmail.com>
2024-02-25 23:55:33 -0800

    Day 87 Topic 1
    This commit includes changes to the Flashzilla app for triggering events repeatedly using a timer.

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,19 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+
     var body: some View {
-        VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("World")
-        }
-        .contentShape(.rect)
-        .onTapGesture {
-            print("VStack tapped!")
-        }
+        Text("Hello, World!")
+            .onReceive(timer) { time in
+                if counter == 5 {
+                    timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                }
+
+                counter += 1
+            }
     }
 }
-
 #Preview {
     ContentView()
 }
