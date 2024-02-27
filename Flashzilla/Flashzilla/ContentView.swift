9cea97affddcd557b6c0f0b9de7936208b982421
Michelle Trinh <trinhmichelle23@gmail.com>
2024-02-26 23:53:09 -0800

    Day 87 Topic 2
    This commit includes changes to the Flashzilla app for receiving notifications when the app moves to the background.

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,22 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
+    @Environment(\.scenePhase) var scenePhase
 
     var body: some View {
-        Text("Hello, World!")
-            .onReceive(timer) { time in
-                if counter == 5 {
-                    timer.upstream.connect().cancel()
-                } else {
-                    print("The time is now \(time)")
+        Text("Hello, world!")
+            .onChange(of: scenePhase) { oldPhase, newPhase in
+                if newPhase == .active {
+                    print("Active")
+                } else if newPhase == .inactive {
+                    print("Inactive")
+                } else if newPhase == .background {
+                    print("Background")
                 }
-
-                counter += 1
             }
     }
 }
+
 #Preview {
     ContentView()
 }
