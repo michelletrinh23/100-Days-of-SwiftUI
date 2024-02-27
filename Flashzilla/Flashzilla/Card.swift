5fcf8d3b7793fa9e7798eabc752e9b8f30ef40a2
Michelle Trinh <trinhmichelle23@gmail.com>
2024-02-27 01:23:14 -0800

    Day 88 Topic 1
    This commit includes changes to the Flashzilla app for designing a single card view.

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Michelle Trinh on 2/27/24.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,23 +7,10 @@
 
 import SwiftUI
 
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
-
+   
     var body: some View {
-        Text("Hello, World!")
-            .padding()
-            .background(reduceTransparency ? .black : .black.opacity(0.5))
-            .foregroundStyle(.white)
-            .clipShape(.capsule)
+        CardView(card: .example)
     }
 }
 
