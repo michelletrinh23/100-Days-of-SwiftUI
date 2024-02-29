640b9349ab672f02ca58448aa51da0afc9efd473
Michelle Trinh <trinhmichelle23@gmail.com>
2024-02-28 22:58:26 -0800

    Day 89 Topic 2
    This commit includes changes to the Flashzilla app for counting down with a timer.

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -15,9 +15,16 @@ extension View {
 }
 
 struct ContentView: View {
+    
     @Environment(\.accessibilityDifferentiateWithoutColor) var
         accessibilityDifferentiateWithoutColor
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
    
     var body: some View {
         ZStack {
@@ -26,6 +33,14 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -57,6 +72,20 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .padding()
                 }
+            }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
             }
         }
     }
