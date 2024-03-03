60a88d9956bb5fc917d2821aceb754a11a927aee
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-03 13:54:02 -0800

    Day 92 Topic 1
    This commit includes additions to the LayoutAndGeometry app for learning how layout works in SwiftUI.

---
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Michelle Trinh on 3/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .background(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Michelle Trinh on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
