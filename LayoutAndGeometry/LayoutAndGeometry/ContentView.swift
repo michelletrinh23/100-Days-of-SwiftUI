e55406a49a2bf468530340fd24bf475619729832
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-03 14:58:15 -0800

    Day 92 Topic 4
    This commit includes changes to the LayoutAndGeometry app for learning about absolute positioning for SwiftUI views.

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,34 +7,11 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    struct MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image(.alesKrivec15949)
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-
-            VStack {
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello, world!")
+            .offset(x: 100, y: 100)
+            .background(.red)
     }
 }
 
