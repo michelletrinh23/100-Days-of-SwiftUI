9f9e3c5521ce9b1fde879246016114fa911143ce
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-03 14:26:17 -0800

    Day 92 Topic 2
    This commit includes changes to the LayoutAndGeometry app for learning alignment and alignment guides.

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .background(.red)
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in Double(position) * -10 }
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
