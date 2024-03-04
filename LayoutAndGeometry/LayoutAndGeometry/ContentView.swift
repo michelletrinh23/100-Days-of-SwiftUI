de7b68acf5a09d474b0e548d76af122f42d2b396
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-04 15:23:18 -0800

    Day 93 Topic 3
    This commit includes changes to the LayoutAndGeometry app for learning ScrollView effects using GeometryReader.

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,41 +7,24 @@
 
 import SwiftUI
 
-struct OuterView: View {
-    var body: some View {
-        VStack {
-            Text("Top")
-            InnerView()
-                .background(.green)
-            Text("Bottom")
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("Left")
-            GeometryReader { proxy in
-                Text("Center")
-                    .background(.blue)
-                    .onTapGesture {
-                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
-                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY)")
-                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
-                    }
-            }
-            .background(.orange)
-            Text("Right")
-        }
-    }
-}
-
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        OuterView()
-            .background(.red)
-            .coordinateSpace(name: "Custom")
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
     }
 }
 
