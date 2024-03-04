a707dfcbd03a1840627e63149bc52ffa624706ac
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-03 23:23:39 -0800

    Day 93 Topic 1
    This commit includes changes to the LayoutAndGeometry app for learning to resize images to fit the screen using GeometryReader.

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .offset(x: 100, y: 100)
-            .background(.red)
+        HStack {
+            Text("IMPORTANT")
+                .frame(width: 200)
+                .background(.blue)
+
+            GeometryReader { proxy in
+                Image(.alesKrivec15949)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.8)
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+            }
+        }
     }
 }
 
