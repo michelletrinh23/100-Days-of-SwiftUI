ee87aaeebf794d4e3153e02a0fdcb8c05290c320
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-18 20:41:58 -0700

    Day 95 ContentView Edits
    This commit includes changes to the ContentView of HighRollers to create the initial user interface.

--- HighRollers/HighRollers/ContentView.swift
+++ HighRollers/HighRollers/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let diceTypes = [4, 6, 8, 10, 12, 20, 100]
+
+    @AppStorage("selectedDiceType") var selectedDiceType = 6
+    @AppStorage("numberToRoll") var numberToRoll = 4
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Type of dice", selection: $selectedDiceType) {
+                        ForEach(diceTypes, id: \.self) { type in
+                            Text("D\(type)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+
+                    Stepper("Number of dice: \(numberToRoll)", value: $numberToRoll, in: 1...20)
+
+                    Button("Roll them!", action: rollDice)
+                }
+            }
+            .navigationTitle("High Rollers")
         }
-        .padding()
+    }
+    
+    func rollDice() {
+
     }
 }
 
