862b063f4ac49dd66b3af4edcfd7f21b1f2d9db8
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-19 23:38:25 -0700

    Day 96 Topic 3
    This commit includes changes to the SnowSeeker app for learning how to use groups as transparent layout containers.

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,21 +7,26 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
+        }
+        .font(.title)
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
+    @Environment(\.horizontalSizeClass) var sizeClass
 
     var body: some View {
-        Text("Hello, World!")
-            .onTapGesture {
-                selectedUser = User()
-                isShowingUser = true
-            }
-            .alert("Welcome", isPresented: $isShowingUser) { }
+        if sizeClass == .compact {
+            VStack(content: UserView.init)
+        } else {
+            HStack(content: UserView.init)
+        }
     }
 }
 
