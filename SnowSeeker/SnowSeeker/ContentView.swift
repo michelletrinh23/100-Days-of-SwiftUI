2a6b47a39853b18bf6ff78661d32849fdf9ff6d2
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-20 00:13:30 -0700

    Day 97 Topic 1
    This commit includes changes to the SnowSeeker app for building a primary list of items.

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,24 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var searchText = ""
-    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
 
     var body: some View {
         NavigationView {
-            List(filteredNames, id: \.self) { name in
-                Text(name)
-            }
-            .searchable(text: $searchText, prompt: "Look for something")
-            .navigationTitle("Searching")
-        }
-    }
+            List(resorts) { resort in
+                NavigationLink {
+                    Text(resort.name)
+                } label: {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 1)
+                        )
 
-    var filteredNames: [String] {
-        if searchText.isEmpty {
-            return allNames
-        } else {
-            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
         }
     }
 }
