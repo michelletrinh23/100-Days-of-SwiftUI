e96dce4086b5feab2d75f68c4c2bf84aa4fc72e7
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-19 23:43:57 -0700

    Day 96 Topic 4
    This commit includes changes to the SnowSeeker app for learning how to make a SwiftUI view searchable.

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,25 +7,25 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna and Arya")
-        }
-        .font(.title)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
 
     var body: some View {
-        if sizeClass == .compact {
-            VStack(content: UserView.init)
+        NavigationView {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
+        }
+    }
+
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            return allNames
         } else {
-            HStack(content: UserView.init)
+            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
         }
     }
 }
