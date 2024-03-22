cd298053ab6c5d474c298b4c6bb6ce6442948cc9
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-22 00:46:12 -0700

    Day 98 Topic 3
    This commit includes changes to the SnowSeeker app for binding an alert to an optional string.

--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -13,6 +13,9 @@ struct ResortView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
     @Environment(\.dynamicTypeSize) var typeSize
     
+    @State private var selectedFacility: Facility?
+    @State private var showingFacility = false
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
@@ -41,14 +44,30 @@ struct ResortView: View {
                     Text("Facilities")
                         .font(.headline)
                     
-                    Text(resort.facilities, format: .list(type: .and))
-                        .padding(.vertical)
+                    HStack {
+                        ForEach(resort.facilityTypes) { facility in
+                            ForEach(resort.facilityTypes) { facility in
+                                Button {
+                                    selectedFacility = facility
+                                    showingFacility = true
+                                } label: {
+                                    facility.icon
+                                        .font(.title)
+                                }
+                            }
+                        }
+                    }
+                    .padding(.vertical)
                 }
                 .padding(.horizontal)
             }
         }
         .navigationTitle("\(resort.name), \(resort.country)")
         .navigationBarTitleDisplayMode(.inline)
+        .alert(selectedFacility?.name ?? "More information", isPresented: $showingFacility, presenting: selectedFacility) { _ in
+        } message: { facility in
+            Text(facility.description)
+        }
     }
 }
 
