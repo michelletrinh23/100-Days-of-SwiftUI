af249b360b22e4a2f3083485b1327b405184b07e
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-22 00:30:37 -0700

    Day 98 Topic 2
    This commit includes changes to the SnowSeeker app for changing a view's layout in response to size classes.

--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ResortView: View {
     let resort: Resort
+    
+    @Environment(\.horizontalSizeClass) var sizeClass
+    @Environment(\.dynamicTypeSize) var typeSize
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
@@ -17,11 +21,18 @@ struct ResortView: View {
                     .scaledToFit()
                 
                 HStack {
-                    ResortDetailsView(resort: resort)
-                    SkiDetailsView(resort: resort)
+                    if sizeClass == .compact && typeSize > .large {
+                        VStack(spacing: 10) { ResortDetailsView(resort: resort) }
+                        VStack(spacing: 10) { SkiDetailsView(resort: resort) }
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                    }
                 }
                 .padding(.vertical)
-                .background(.primary.opacity(0.1))
+                .background(Color.primary.opacity(0.1))
+                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
+
                 
                 Group {
                     Text(resort.description)
