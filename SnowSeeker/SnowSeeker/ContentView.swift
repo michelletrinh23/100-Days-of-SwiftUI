b27434f12047513a24c40c9dc8774617ea2884b0
Michelle Trinh <trinhmichelle23@gmail.com>
2024-03-23 00:02:16 -0700

    Day 98 Topic 4
    This commit includes changes to the SnowSeeker app for letting the user mark favorites.

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
 
+    @StateObject var favorites = Favorites()
     @State private var searchText = ""
     
     var body: some View {
@@ -18,24 +19,33 @@ struct ContentView: View {
                 NavigationLink {
                     ResortView(resort: resort)
                 } label: {
-                    Image(resort.country)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 40, height: 25)
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 5)
-                        )
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(.black, lineWidth: 1)
-                        )
-                    
-                    VStack(alignment: .leading) {
-                        Text(resort.name)
-                            .font(.headline)
+                    HStack {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 5)
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth: 1)
+                            )
                         
-                        Text("\(resort.runs) runs")
-                            .foregroundColor(.secondary)
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                        }
+                        
+                        if favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                                .accessibilityLabel("This is a favorite resort")
+                                .foregroundColor(.red)
+                        }
                     }
                 }
             }
@@ -44,6 +54,7 @@ struct ContentView: View {
             
             WelcomeView()
         }
+        .environmentObject(favorites)
     }
     
     var filteredResorts: [Resort] {
--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -13,6 +13,8 @@ struct ResortView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
     @Environment(\.dynamicTypeSize) var typeSize
     
+    @EnvironmentObject var favorites: Favorites
+    
     @State private var selectedFacility: Facility?
     @State private var showingFacility = false
     
@@ -56,6 +58,16 @@ struct ResortView: View {
                                 }
                             }
                         }
+                        
+                        Button(favorites.contains(resort) ? "Remove from Favorites" : "Add to Favorites") {
+                            if favorites.contains(resort) {
+                                favorites.remove(resort)
+                            } else {
+                                favorites.add(resort)
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .padding()
                     }
                     .padding(.vertical)
                 }
